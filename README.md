6264b445145bf981914a3fc4f09bc32075aaf97d
Jim Kalafut <jim@kalafut.net>
2015-12-09 07:52:18 -0800

    Added basic search

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # fff
 Fast File Finder
+
+Simple file finder in Go. Ignores .git folders.
+
+# Usage
+`fff [search]`
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	flag.Parse()
+	exclude := map[string]bool{}
+	scan(".", flag.Arg(0), exclude)
+}
+
+func scan(root, search string, exclude map[string]bool) {
+	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() && info.Name() == ".git" {
+			return filepath.SkipDir
+		}
+		if !exclude[filepath.Ext(path)] && strings.Contains(path, search) {
+			fmt.Println(path)
+		}
+		return nil
+	})
+}
